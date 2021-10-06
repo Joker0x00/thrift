84cfe32fc421e6339ee159e45656b13c8ec41fb8
wy <1811170468@qq.com>
2021-10-06 20:49:29 +0800

    finished client.py

--- game/src/client.py
+++ game/src/client.py
@@ -6,8 +6,9 @@ from thrift.transport import TSocket
 from thrift.transport import TTransport
 from thrift.protocol import TBinaryProtocol
 
+from sys import stdin
 
-def main():
+def operate(op, user_id, username, score):
     # Make socket
     transport = TSocket.TSocket('localhost', 9090)
 
@@ -19,16 +20,22 @@ def main():
 
     # Create a client to use the protocol encoder
     client = Match.Client(protocol)
-
+    
+    user = User(user_id, username, score)
     # Connect!
     transport.open()
-
-    user = User(1, "wy", 1500)
-
-    client.add_user(user, "")
+    if(op == "add"):
+        client.add_user(user, "")
+    elif op == "remove":
+        client.remove_user(user, "")
 
     # Close!
     transport.close()
+
+def main():
+    for line in stdin:
+        op, user_id, username, score = line.split(' ')
+        operate(op, int(user_id), username, int(score))
 
 
 if __name__ == "__main__" :
